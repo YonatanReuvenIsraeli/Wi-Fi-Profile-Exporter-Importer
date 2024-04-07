d441b477580f153345e5e17d59448bf5d2d64829
Yonatan Israeli <yonirisraeli@gmail.com>
2024-04-07 17:13:25 -0400

    Update Wi-Fi Profile Exporter-Importer.bat

--- Wi-Fi Profile Exporter-Importer.bat
+++ Wi-Fi Profile Exporter-Importer.bat
@@ -1,19 +1,8 @@
 @echo off
 setlocal
 title Wi-Fi Profile Exporter/Importer
-echo Wi-Fi Profile Exporter/Importer v1.0.0
-echo.
-echo READ DISCLAIMER ^-^-^> THIS IS FOR EDUCATIONAL PURPOSES ONLY! DO NOT USE THIS ON SOMEONE ELSES PC WITHOUT THEIR EXPLICIT PERMISSION! THIS IS A HACK! CONTINUE AT YOUR OWN RISK! WE HOLD NO RESPONSIBILITY FOR ANYTHING THAT HAPPEND, IS HAPPENING, AND/OR WILL HAPPEN BECAUSE OF THIS BATCH FILE!
-goto Disclaimer
-
-:Disclaimer
-echo.
-set Disclaimer=
-set /p Disclaimer="Do you agree to the Disclaimer? (Yes/No) "
-if /i "%Disclaimer%"=="Yes" goto Start
-if /i "%Disclaimer%"=="No" goto Exit
-echo Invalid Syntax!
-goto Disclaimer
+echo Wi-Fi Profile Exporter/Importer v1.1.0
+goto Start
 
 :Start
 echo.
