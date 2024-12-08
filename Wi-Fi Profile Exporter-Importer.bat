fe20036dedb34b60bd9cd20d45d9df110c24daad
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-07 20:41:53 -0500

    Update Wi-Fi Profile Exporter-Importer.bat

--- Wi-Fi Profile Exporter-Importer.bat
+++ Wi-Fi Profile Exporter-Importer.bat
@@ -2,7 +2,7 @@
 setlocal
 title Wi-Fi Profile Exporter/Importer
 echo Program Name: Wi-Fi Profile Exporter/Importer
-echo Version: 1.2.7
+echo Version: 1.2.8
 echo Developer: @YonatanReuvenIsraeli
 echo Website: https://www.yonatanreuvenisraeli.dev
 echo License: GNU General Public License v3.0
@@ -15,7 +15,6 @@ set /p Export/Import="Do you want to export Wi-Fi profiles or import Wi-Fi profi
 if /i "%Export/Import%"=="Export" goto "Export"
 if /i "%Export/Import%"=="Import" goto "Import"
 echo Invalid syntax!
-echo.
 goto "Start"
 
 :"Export"
@@ -33,10 +32,10 @@ set FullPath=
 set /p FullPath="What folder to export the Wi-Fi profiles to? "
 if not exist "%FullPath%" goto "NotExist"
 if /i "%Export%"=="All" goto "All"
-if /i "%Export%"=="Some" goto "Some1"
+if /i "%Export%"=="Some" goto "Some"
 
 :"NotExist"
-echo "%FullPath%" Does Not Exist!
+echo "%FullPath%" does Not Exist!
 goto "FullPath"
 
 :"All"
@@ -45,27 +44,18 @@ echo.
 echo All Wi-Fi profiles exported to %FullPath%! Press any key to close this batch file.
 goto "Done"
 
-:"Some1"
+:"Some"
 netsh wlan show profile
-goto "Some2"
-
-:"Some2"
-echo.
 set Profile=
 set /p Profile="Which Wi-Fi profile do you want to export? "
 netsh wlan export profile name="%Profile%" folder="%FullPath%" key=clear
-if not "%errorlevel%"=="0" goto "Error"
 goto "AnotherExport"
-
-:"Error"
-echo Invalid profile name!
-goto "Some2"
 
 :"AnotherExport"
 echo.
 set AnotherExport=
 set /p AnotherExport="Do you want to export another Wi-Fi profile? (Yes/No) "
-if /i "%AnotherExport%"=="Yes" goto "Some2"
+if /i "%AnotherExport%"=="Yes" goto "Some"
 if /i "%AnotherExport%"=="No" goto "AnotherDone"
 echo Invalid syntax!
 goto "AnotherExport"
@@ -75,13 +65,17 @@ echo.
 set ProfilePath=
 set /p ProfilePath="What is the full path of your Wi-Fi profile? Key must be set to clear. If the Wi-fi profile was exported useing this batch file then key would've been be set to clear. "
 netsh wlan add profile "%ProfilePath%"
-if not "%errorlevel%"=="0" goto "Import"
+if not "%errorlevel%"=="0" goto "Error"
 goto "AnotherImport"
+
+:"Error"
+echo There has been an error! Key may have not be set to clear. "%ProfilePath%" may not be a Wi-Fi profile or not exist. You can try again.
+goto "Import"
 
 :"AnotherImport"
 echo.
 set Another=
-set /p Another="Wi-Fi profile imported! Do you want to import antoher Wi-Fi profile? (Yes/No) "
+set /p Another="Wi-Fi profile imported! Do you want to import another Wi-Fi profile? (Yes/No) "
 if /i "%Another%"=="Yes" goto "Import"
 if /i "%Another%"=="No" goto "AnotherDone"
 echo Invalid syntax!
@@ -96,5 +90,5 @@ exit
 endlocal
 echo.
 echo Press any key to close this batch file.
-pause > nul
+pause > nul 2>&1
 exit
